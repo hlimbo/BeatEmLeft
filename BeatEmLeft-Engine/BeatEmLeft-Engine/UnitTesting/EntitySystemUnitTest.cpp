53f053552a42580c279dc3c36f19289f73c484a8
hlimbo <hlimbo@DESKTOP-QV21ATA.localdomain>
2017-06-29 03:31:58 -0700

    WIP EntitySystem pt2

--- BeatEmLeft-Engine/BeatEmLeft-Engine/UnitTesting/EntitySystemUnitTest.cpp
+++ BeatEmLeft-Engine/BeatEmLeft-Engine/UnitTesting/EntitySystemUnitTest.cpp
@@ -17,7 +17,7 @@ TEST(CreateEntity, CreateEntity0)
 	EntitySystem system;
 
 	Entity e1{ "grass-tile" };
-	int id = system.CreateEntity(&e1);
+	int id = system.CreateEntity(e1);
 	EXPECT_EQ(e1.type, "grass-tile");
 
 	EXPECT_EQ(id, 0);
@@ -53,18 +53,18 @@ TEST(CreateEntity, CreateEntity1)
 	EntitySystem system;
 
 	Entity e1{ "cat" };
-	int catID = system.CreateEntity(&e1);
+	int catID = system.CreateEntity(e1);
 	EXPECT_EQ(catID, 0);
 	EXPECT_EQ(catID, e1.id);
 
 	Entity e2{ "cat" };
-	int newID = system.CreateEntity(&e2);
+	int newID = system.CreateEntity(e2);
 	EXPECT_EQ(newID, 1);
 	EXPECT_EQ(newID, e2.id);
 	EXPECT_NE(e1.id, e2.id);
 
 	Entity e3{ "giraffe" };
-	int gID = system.CreateEntity(&e3);
+	int gID = system.CreateEntity(e3);
 	EXPECT_EQ(gID, e3.id);
 }
 
@@ -73,11 +73,11 @@ TEST(CreateEntity, DontDuplicateEntityWithSameID)
 	EntitySystem system;
 
 	Entity e1{ "cat" };
-	int status = system.CreateEntity(&e1);
+	int status = system.CreateEntity(e1);
 	EXPECT_EQ(e1.id, 0);
 	EXPECT_EQ(status, e1.id);
 
-	status = system.CreateEntity(&e1);
+	status = system.CreateEntity(e1);
 	EXPECT_EQ(e1.id, 0);
 	EXPECT_EQ(status, -1);
 }
@@ -90,7 +90,7 @@ TEST(CreateEntity, MultipleEntities)
 	for (int i = 0;i < 10; ++i)
 	{
 		e[i].type = "dog";
-		int dogID = system.CreateEntity(&e[i]);
+		int dogID = system.CreateEntity(e[i]);
 		EXPECT_EQ(dogID, e[i].id);
 	}
 }
@@ -103,7 +103,7 @@ TEST(EntityCount, EntityCountSimple)
 	for (int i = 0;i < 10;++i)
 	{
 		e[i].type = "giraffe";
-		system.CreateEntity(&e[i]);
+		system.CreateEntity(e[i]);
 	}
 
 	for (int i = 0;i < 10;++i)
@@ -114,21 +114,86 @@ TEST(EntityCount, EntityCountSimple)
 	EXPECT_EQ(system.EntityCount("giraffe"), 10);
 }
 
-TEST(AddressCheck, EntityTest)
+
+TEST(RemoveEntity, RemoveEntity1)
 {
 	EntitySystem system;
 	Entity e[10];
 	for (int i = 0;i < 10;++i)
 	{
-		e[i].type = "donkey";
-		system.CreateEntity(&e[i]);
+		e[i].type = "rice";
+		system.CreateEntity(e[i]);
+	}
+	
+	EXPECT_EQ(system.EntityCount("rice"), 10);
+	vector<Entity>* entities = system.GetEntities("rice");
+
+	for (int i = 0;i < system.EntityCount("rice"); ++i)
+	{
+		//cout << system.entitiesByType["rice"][i].name << endl;
+		//cout << ((*entities)[i]).name<< endl;// alternate way of doing the same thing below
+		cout << entities->at(i).name << endl;
 	}
 
-	vector<Entity*> donkeys = system.GetEntities("donkey");
-	for (int i = 0;i < 10;++i)
+	int idToRemove = system.RemoveEntity("rice", 4);
+	EXPECT_EQ(idToRemove, 4);
+	cout << endl << endl;
+	EXPECT_EQ(system.EntityCount("rice"), 9);
+	for (int i = 0;i < system.EntityCount("rice"); ++i)
 	{
-		printf("donkey: %d | e: %d\n", &donkeys[i], &e[i]);
+		//cout << system.entitiesByType["rice"][i].name << endl;
+		cout << entities->at(i).name << endl;
 	}
+
+	cout << endl << endl;
+
+	idToRemove = system.RemoveEntity("rice", 4);
+	EXPECT_EQ(idToRemove, -1);
+	EXPECT_EQ(system.EntityCount("rice"), 9);
+	for (int i = 0;i < system.EntityCount("rice"); ++i)
+	{
+		//cout << system.entitiesByType["rice"][i].name << endl;
+		cout << entities->at(i).name << endl;
+	}
+
+	cout << endl << endl;
+
+	idToRemove = system.RemoveEntity("rice", 0);
+	EXPECT_EQ(idToRemove, 0);
+	EXPECT_EQ(system.EntityCount("rice"), 8);
+	for (int i = 0;i < system.EntityCount("rice"); ++i)
+	{
+		//cout << system.entitiesByType["rice"][i].name << endl;
+		cout << entities->at(i).name << endl;
+	}
+
+}
+
+TEST(RemoveEntityType, RemoveEntityType1)
+{
+	EntitySystem system;
+	system.AddEntityType("cheese");
+	system.AddEntityType("bread");
+	system.AddEntityType("donut");
+
+	Entity e[20];
+	for (int i = 0;i < 20;++i)
+	{
+		e[i].type = "bread";
+		system.CreateEntity(e[i]);
+	}
+
+	EXPECT_TRUE(system.RemoveEntityType("bread"));
+	EXPECT_FALSE(system.RemoveEntityType("bread"));
+	EXPECT_FALSE(system.RemoveEntityType("bchk"));
+	EXPECT_EQ(system.TypesCount(), 2);
+
+	EXPECT_TRUE(system.RemoveEntityType("donut"));
+	EXPECT_EQ(system.TypesCount(), 1);
+
+	EXPECT_TRUE(system.RemoveEntityType("cheese"));
+	EXPECT_EQ(system.TypesCount(), 0);
+
 }
 
 int main(int argc, char* argv[])
