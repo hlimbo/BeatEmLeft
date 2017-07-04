1053ca72041b3e258aab761dfafc2876cef46f9a
hlimbo <hlimbo@github.com>
2017-07-04 01:18:04 -0700

    almost finished ECS

--- BeatEmLeft-Engine/BeatEmLeft-Engine/Systems/ComponentSystem.h
+++ BeatEmLeft-Engine/BeatEmLeft-Engine/Systems/ComponentSystem.h
@@ -15,17 +15,30 @@ class ComponentSystem
 {
 public:
 	ComponentSystem() {}
+	ComponentSystem(string type) { this->type = type; }
 	~ComponentSystem();
 
-	void AddComponent(int id, Component* component);
+	bool AddComponent(int id, Component* component);
+	Component* GetComponent(int id);
 
-	//this level of granularity maybe needed if I plan to re-register
-	//components to different entity ids...
+	//Currently, there is not component type checking before we can
+	//add the component to the system.. componentSystem can hold
+	//a mixed bag of components...
+	//Design choice: don't want to hold a mixed bag of components
+	//in the componentSystem because each component is mapped to
+	//a single entity id which can be reused exactly once per
+	//component system.
+
+	//current design flow: only 1 entity id can be assigned per component type
+	//e.g. an entity cannot have more than 1 render component
 
 	//returns the component to properly delete to prevent resource leak
 	//throws an exception if id is invalid
 	Component* RemoveComponent(int id);
-	bool DeleteComponent(Component* component);
+
+	int GetComponentCount() { return components.size();  }
+	bool IsEmpty() { return components.empty();  }
+	string GetType() { return type; }
 
 private:
 	//key = component type, value = map<key = id,value = component>
@@ -34,6 +47,7 @@ private:
 	//another option is to create an array of componentsystems in ECS to hold labeled by type
 	//key = entity id, value = Component* (which can be any kind of component derived from the Component class)
 	unordered_map<int,Component*> components;
+	string type;
 
 	//component type vs entity type 
 	//entity type does not need to depend on component type
