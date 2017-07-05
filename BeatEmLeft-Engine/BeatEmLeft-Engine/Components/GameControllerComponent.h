20909b7cbd28d5b6310077512ab503526231d7b5
hlimbo <hlimbo@github.com>
2017-07-04 19:48:44 -0700

    Add interactive gamecontroller unit test. Add gamecontroller analog input reading

--- BeatEmLeft-Engine/BeatEmLeft-Engine/Components/GameControllerComponent.h
+++ BeatEmLeft-Engine/BeatEmLeft-Engine/Components/GameControllerComponent.h
@@ -3,6 +3,7 @@
 #include "../Systems/Component.h"
 #include "SDL2/SDL_gamecontroller.h"
 #include <unordered_map>
+#include <Vect2.h>
 
 enum class ButtonStates
 {
@@ -21,14 +22,25 @@ public:
 	virtual void Init();
 	virtual void Update(float deltaTime);
 
-	bool buttonPressed(std::string buttonName);
-	bool buttonHeld(std::string buttonName);
-	bool buttonReleased(std::string buttonName);
+	//the state of each button press gets updated regularly via Update call
+	//make sure to check for the status of the button on every update 
+	bool ButtonPressed(std::string buttonName);
+	bool ButtonHeld(std::string buttonName);
+	bool ButtonReleased(std::string buttonName);
+
+	Vect2 LeftAnalogInput();
+	Vect2 RightAnalogInput();
+
 private:
 	SDL_GameController* controller;
 	std::unordered_map<std::string, ButtonStates> buttonMap;
+	Vect2 leftAnalogInput;
+	Vect2 rightAnalogInput;
+
 	int pressedTime;
 	int timeHeldDelta;//the number of milliseconds required to consider a button press to be a button held down.
+	
+	Vect2 adjustAnalogInput(int ax, int ay);
 };
 
 #endif
