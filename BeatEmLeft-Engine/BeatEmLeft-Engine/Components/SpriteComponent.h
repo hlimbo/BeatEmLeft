eab485fd26cd73e896d6e223440aff699b048eb0
hlimbo <hlimbo@github.com>
2017-07-06 03:53:12 -0700

    Add Template function GetComponent in ComponentManager

---
+++ BeatEmLeft-Engine/BeatEmLeft-Engine/Components/SpriteComponent.h
@@ -0,0 +1,37 @@
+#ifndef SPRITE_COMPONENT_H
+#define SPRITE_COMPONENT_H
+
+#include "../Systems/Component.h"
+
+struct SDL_Texture;
+struct SDL_Renderer;
+
+class SpriteComponent :
+	public Component
+{
+public:
+	SpriteComponent(std::string type);
+	virtual ~SpriteComponent();
+
+	//may need to rewrite component base class to not include
+	//inheritable functions via virtual keyword.
+	virtual void Init();
+	virtual void Update(float deltaTime);
+
+	//helper function
+	bool LoadTexture(SDL_Renderer* render, std::string filePath);
+
+	//modify this width and height to see changes in size of image on screen
+	int width, height;
+	//pixel coordinates of image
+	int x, y;
+
+private:
+	SDL_Texture* texture;
+	//source width and height of image after loaded into the component
+	int src_w, src_h;
+
+};
+
+#endif
+
