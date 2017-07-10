5d50477b40fde7d9eb02502fbdefc3d9c092e2b0
hlimbo <hlimbo@github.com>
2017-07-10 01:30:53 -0700

    Rename KeyboardInputComponent to KeyboardController and remove inheritance from Component class

--- BeatEmLeft-Engine/BeatEmLeft-Engine/Systems/RenderSystem.cpp
+++ BeatEmLeft-Engine/BeatEmLeft-Engine/Systems/RenderSystem.cpp
@@ -135,12 +135,7 @@ void RenderSystem::Update(float deltaTime, SDL_Renderer* render)
 			
 			//benefit of this approach is that I have more control over how I want to round my floating coordinates to
 			//negative of this approach: I have to write a little bit more code to do this...
-		/*	float diffx = transform->position.x - floorf(transform->position.x);
-			float diffy = transform->position.y - floorf(transform->position.y);
-
-			sprite->x = (diffx > 0.5f) ? (int)ceilf(transform->position.x) : (int)floorf(transform->position.x);
-			sprite->y = (diffy > 0.5f) ? (int)ceilf(transform->position.y) : (int)floorf(transform->position.y);*/
-			//sprite->Draw(render);
+			//SDL_Point newPoints = getFloatToIntegerCoordinates(transform->position);
 
 			//temp code ~ needs to be refactored...
 			if (backgroundID == *it)
@@ -179,7 +174,7 @@ void RenderSystem::Update(float deltaTime, SDL_Renderer* render)
 					x---------------x
 				*/
 
-				//this is an optimization for rendering since rendering many textures each frame is expensive
+				//this is an optimization for rendering since rendering many textures offscreen each frame is expensive
 				if (screenCoords.x >= 0 && screenCoords.x < camera.w
 					&& screenCoords.y >= 0 && screenCoords.y < camera.h)
 				{
@@ -227,57 +222,3 @@ SDL_Point RenderSystem::getFloatToIntegerCoordinates(Vect2 position)
 	return p;
 }
 
-//in systems class:
-//unordered_map<std::string,ComponentManager*> managers;
-
-//in systems::update(deltaTime)
-/*
-vector<int> ids = entitySystem->GetIDs();
-
-ComponentManager* transformManager = managers.at("transformManager");
-ComponentManager* spriteManager = managers.at("spriteManager");
-
-for(auto it = ids.begin();it != ids.end();++it)
-{
-//there's probably a cleaner way of doing this.. not sure how I would achieve this..
-//e.g. transformManager->GetComponent<TransformComponent>(*it);
-
-TransformComponent* transform =
-dynamic_cast<TransformComponent*>(transformManager->GetComponent(*it));
-
-SpriteComponent* sprite =
-dynamic_cast<SpriteComponent*>(spriteManager->GetComponent(*it));
-
-if(transform != nullptr && sprite != nullptr)
-{
-//here we know that it belongs to the system
-//without the need of type checking since
-//the manager already ensures all components added to it
-//are of a single type e.g. ComponentManager A only holds
-//TransformComponents
-
-//convert floating point coordinates to screen integer coordinates
-float diffX = transform->x - floor(transform->x);
-float diffY = transform->y - floor(transform->y);
-
-sprite->x = (diffX > 0.5) ? (int)ceil(transform->x) : (int)floor(transform->x);
-sprite->y = (diffY > 0.5) ? (int)ceil(transform->y) : (int)floor(transform->y);
-
-
-//might not need to update the individual components here...
-//only access its data here and change it if necessary and let the component managers
-//in ecs handle  the updates (drawing image to screen to proper coordinates).
-sprite->draw(//NEED RENDERER HERE//);
-..SDL_RenderCopy(//NEED RENDERER HERE//,sprite->texture?,NULL,&sprite->bounds?);
-
-//may not need to call since ecs calls every update function of every component
-//sprite->update(deltaTime);//sprite->update() will just call SDL_RenderCopy();
-}
-}
-
-//** (may do this one instead) One way to fix this
-//easily is to just pass in an SDL_Renderer* when the
-//Render system is constructed..
-SDL_RenderPresent(//NEED RENDERER HERE//);
-SDL_RenderClear(//NEED RENDERER HERE//);
-*/
