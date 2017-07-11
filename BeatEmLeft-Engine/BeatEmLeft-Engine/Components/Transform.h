a6c6224f9f131a91fccde82531998e764dc477dd
hlimbo <hlimbo@github.com>
2017-07-11 13:22:12 -0700

    Convert ComponentManager into a template class.Remove Component inheritance from SpriteComponent and TransformComponent. Rename SpriteComponent to Sprite. Rename TransformComponent to Transform. Lots of Refactoring in ECS, RenderingSystem

--- BeatEmLeft-Engine/BeatEmLeft-Engine/Components/Transform.h
+++ BeatEmLeft-Engine/BeatEmLeft-Engine/Components/Transform.h
@@ -1,22 +1,15 @@
 #ifndef TRANSFORM_COMPONENT_H
 #define TRANSFORM_COMPONENT_H	
-#include "../Systems/Component.h"
 #include <Vect2.h>
 
 //use this component to modify the game world coordinates and properties
 //of the entity that registered to this component
-class TransformComponent :
-	public Component
+struct Transform
 {
-public:
-	TransformComponent(std::string type) : Component(type) {}
-	virtual ~TransformComponent() {}
-
-	virtual void Init();
-	virtual void Update(float deltaTime);
-
+	Transform() { type = typeid(Transform).name(); }
 	Vect2 position;
 	Vect2 scale;
+	const char* type;
 };
 
 #endif
