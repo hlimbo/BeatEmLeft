ece6fab38e53031993157a23900087b89e8af0ef
hlimbo <hlimbo@users.noreply.github.com>
2017-07-22 21:11:53 -0700

    WIP overlap box function in CollisionQuery

--- BeatEmLeft-Engine/BeatEmLeft-Engine/Systems/RenderSystem.h
+++ BeatEmLeft-Engine/BeatEmLeft-Engine/Systems/RenderSystem.h
@@ -27,13 +27,14 @@ public:
 	void Update(SDL_Renderer* render);
 	void Draw(SDL_Renderer* render);
 
+	SDL_Rect camera;//may need to pull out to use in other systems..
+
 private:
 	//do not need to free these pointers since they get freed in ecs
 	ECS* ecs;
 	//obtained from ecs
 	ComponentManager<Transform>* transformManager;
 	ComponentManager<Sprite>* spriteManager;
-	SDL_Rect camera;//may need to pull out to use in other systems..
 
 	//converts floating point coordinates to integer point coordinates
 	SDL_Point getFloatToIntegerCoordinates(Vect2 position);
