b100cc97ea868c3a281e0c4adc43491ff9c14743
hlimbo <hlimbo@users.noreply.github.com>
2017-07-26 20:17:01 -0700

    Add SetAlpha to TextureStore class

--- BeatEmLeft-Engine/BeatEmLeft-Engine/Utility/TextureStore.h
+++ BeatEmLeft-Engine/BeatEmLeft-Engine/Utility/TextureStore.h
@@ -1,6 +1,7 @@
 #ifndef TEXTURE_STORE_H
 #define TEXTURE_STORE_H
 #include "TextureLoader.h"
+#include <SDL2/SDL_render.h>
 #include <unordered_map>
 #include <string>
 
@@ -17,7 +18,8 @@ public:
 	bool Free(const std::string fileName);
 	SDL_Texture* Get(const std::string fileName);
 
-
+	//Note: alpha is clamped between 0-255 inclusive
+	void SetAlpha(const std::string fileName, Uint8 alpha, SDL_BlendMode blendMode = SDL_BLENDMODE_BLEND);
 
 private:
 	SDL_Renderer* render;//a pointer to the game window's renderer
